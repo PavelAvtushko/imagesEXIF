7b2f9393db4ffeb5ae67e74b6111e006a3ce4bf7
Pavel Autushka <Pavel_Autushka@epam.com>
2017-03-17 20:51:06 +0300

    createGPS data

--- model.js
+++ model.js
@@ -1,10 +1,41 @@
-    var imageCollection = new Map();
+//import EXIF
 
-    function NewImage(src, coord) {
-        this.id = 'img' + Date.now();
-        this.img = new Image();
-        this.img.src = src;
-        this.img.dataset['id'] = this.id;
-        this.description = '';
-        this.coord = coord;
+var imageCollection = new Map();
+
+function NewImage(src) {
+    this.id = 'img' + Date.now();
+    this.img = new Image();
+    this.img.src = src;
+    this.img.dataset['id'] = this.id;
+    this.description = '';
+    this.coord = extractGPSData(this.img) || {};
+    this.exifData = extractExifData(this.img) || "";
+}
+
+function extractGPSData(img) {
+    var result;
+    EXIF.getData(img, () => {
+        var yCoord  = EXIF.getTag(img, 'GPSLongitude') || null;
+        var xCoord = EXIF.getTag(img, 'GPSLatitude') || null;
+        result = {
+                    x: coordinatesToDecimal(xCoord),
+                    y: coordinatesToDecimal(yCoord) 
+                }
+    });
+    return result;
+};
+
+function extractExifData(img) {
+
+        var allMetaData = EXIF.getAllTags(img);
+        return JSON.stringify(allMetaData, null, "\t");
+};
+
+function coordinatesToDecimal(number) {
+    if (number) {
+        return number[0].numerator + number[1].numerator /
+            (60 * number[1].denominator) + number[2].numerator /
+            (3600 * number[2].denominator);
     }
+    return;
+};
